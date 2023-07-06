7245d35532f67275cb6fa600f322641eb5fab18e
Anderson Coimbra <andersonmegax@hotmail.com>
2023-07-06 00:04:13 -0300

    calculadora via console

--- projetos/dados_console/bin/dados_console.dart
+++ projetos/dados_console/bin/dados_console.dart
@@ -2,21 +2,47 @@ import 'package:dados_console/dados_console.dart' as dados_console;
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print('Informe  a primeira nota: ');
-  var entradaNota1 = stdin.readLineSync();
-  print('Informe  a segunda nota: ');
-  var entradaNota2 = stdin.readLineSync();
-  print('Informe  a terceira nota: ');
-  var entradaNota3 = stdin.readLineSync();
-  print('Informe  a quarta nota: ');
-  var entradaNota4 = stdin.readLineSync();
+  print('Bem vindo a nossa calculadora: ');
+  
+  print('Digite o primeiro número: ');
+  var line = stdin.readLineSync();
+  var number1 = double.parse(line ?? '0');
 
-  var nota1 = double.parse(entradaNota1 ?? "0" );
-  var nota2 = double.parse(entradaNota2 ?? "0" );
-  var nota3 = double.parse(entradaNota3 ?? "0" );
-  var nota4 = double.parse(entradaNota4 ?? "0" );
+  print('Digite o segundo número: ');
+  line = stdin.readLineSync();
+  var number2 = double.parse(line ?? '0');
 
-  var media = (nota1 + nota2 + nota3 + nota4) / 4;
+  print('Digite a operação ( +, -, x, /): ');
+  line = stdin.readLineSync();
+  var operation = line ?? '';
 
-  print('A média é: $media');
+  switch(operation) {
+    case '+':
+      print('O resultado é: ${number1 + number2}');
+      break;
+    case '-':
+      print('O resultado é: ${number1 - number2}');
+      break;
+    case 'x':
+      print('O resultado é: ${number1 * number2}');
+      break;
+    case '/':
+      print('O resultado é: ${number1 / number2}');
+      break;
+    default:
+      print('Operação inválida');
+  }
+
+  if(operation == '+') {
+    print('O resultado é: ${number1 + number2}');
+  } else if(operation == '-') {
+    print('O resultado é: ${number1 - number2}');
+  } else if(operation == 'x') {
+    print('O resultado é: ${number1 * number2}');
+  } else if(operation == '/') {
+    print('O resultado é: ${number1 / number2}');
+  } else {
+    print('Operação inválida');
+  }
+
 }
